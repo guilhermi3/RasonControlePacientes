949b3e6805eb32ce0d6f69fb6bb9670cbc711e49
Guilherme Sandmann <guilherme_sandmann@hotmail.com>
2019-05-21 21:41:51 -0300

    Etapa 2

--- app/src/main/java/br/org/catolicasc/rasoncontrolepacientes/MainActivity.java
+++ app/src/main/java/br/org/catolicasc/rasoncontrolepacientes/MainActivity.java
@@ -1,41 +1,64 @@
 package br.org.catolicasc.rasoncontrolepacientes;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lvPaciente = findViewById(R.id.lvPaciente);
-
-
-        lvPaciente.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, NovoPaciente.class);
-                    //    Integer.valueOf(((TextView)view.findViewById(R.id.etNome)).getText().toString());
-                startActivity(intent);
-            }
-        });
 
         Button btnAdd = findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NovoPaciente.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, NovoPaciente.class);
+                startActivity(it);
             }
         });
+
+
+        ////
+
+        ListView lvPacientes = findViewById(R.id.lvPaciente);
+
+        DAL dal = new DAL(this);
+        Cursor cursor = dal.loadAll();
+
+        String[] fields = new String[] {CreateDatabase.ID, CreateDatabase.NOME};
+        int[] ids = {R.id.tvId, R.id.tvTitle};
+
+        //Log.d(TAG, "onCreate: id " + cursor.getInt(0) + " title " + cursor.getString(1));
+        Log.d(TAG, "onCreate: " + cursor.getCount());
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(MainActivity.this,
+                R.layout.paciente_layout, cursor, fields, ids, 0);
+
+        lvPacientes.setAdapter(adapter);
+
+
+        ////
+
+
     }
+
+
 }
+
--- app/src/main/java/br/org/catolicasc/rasoncontrolepacientes/NovoPaciente.java
+++ app/src/main/java/br/org/catolicasc/rasoncontrolepacientes/NovoPaciente.java
@@ -1,42 +1,138 @@
 package br.org.catolicasc.rasoncontrolepacientes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-
-
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NovoPaciente extends AppCompatActivity {
+    private TextView novoPaciente;
+    private TextView nome;
+    private TextView idade;
+    private TextView leococitos;
+    private TextView glicemia;
+    private TextView ast;
+    private TextView ldh;
+    private TextView pontuacao;
+    private TextView mortalidade;
+    private EditText campoNome;
+    private EditText campoIdade;
+    private EditText campoLeococitos;
+    private EditText campoGlicemia;
+    private EditText campoAst;
+    private EditText campoLdh;
+    private CheckBox litiaseBiliar;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_paciente);
 
-        Button btnUpadate = findViewById(R.id.btnUpadate);
+        /////
+        Button adc = findViewById(R.id.btnUpdate);
+        Button voltar = findViewById(R.id.btVoltar);
+        litiaseBiliar = findViewById(R.id.checkBox);
+        campoNome = findViewById(R.id.etNome);
+        campoIdade = findViewById(R.id.etIdade);
+        campoLeococitos = findViewById(R.id.etLeucocitos);
+        campoGlicemia = findViewById(R.id.etGlicemia);
+        campoAst = findViewById(R.id.etAST);
+        campoLdh = findViewById(R.id.etLDH);
+        pontuacao = findViewById(R.id.tvPontuação);
+        mortalidade = findViewById(R.id.tvMortalidade);
+        pontuacao.setText("");
+        mortalidade.setText("");
 
 
-        btnUpadate.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener trocaTela = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText etNome = findViewById(R.id.etNome);
-                EditText etIdade = findViewById(R.id.etIdade);
-                EditText etLeucocitos = findViewById(R.id.etLeucocitos);
-                EditText etGlicemia = findViewById(R.id.etGlicemia);
-                EditText etAST = findViewById(R.id.etAST);
-                EditText etLDH = findViewById(R.id.etLDH);
-
-
-                String nome = etNome.getText().toString();
-                String idade = etIdade.getText().toString();
-                String leucocitos = etLeucocitos.getText().toString();
-                String glicemia = etGlicemia.getText().toString();
-                String ast = etAST.getText().toString();
-                String ldh = etLDH.getText().toString();
-
-
+                Intent it = new Intent(NovoPaciente.this, MainActivity.class);
+                startActivity(it);
             }
+        };
+        voltar.setOnClickListener(trocaTela);
+
+        /////
+        adc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DAL dal = new DAL(NovoPaciente.this);
+                String nome = campoNome.getText().toString();
+                Double idade = Double.valueOf(campoIdade.getText().toString());
+                Double leococitos = Double.valueOf(campoLeococitos.getText().toString());
+                Double glicemia = Double.valueOf(campoGlicemia.getText().toString());
+                Double ast = Double.valueOf(campoAst.getText().toString());
+                Double ldh = Double.valueOf(campoLdh.getText().toString());
+                int qtd = 0;
+                int mor = 0;
+
+                if (dal.insert(nome, idade, leococitos,glicemia, ast,ldh)) {
+                    Toast.makeText(NovoPaciente.this,
+                            "Registro Inserido com sucesso!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(NovoPaciente.this,
+                            "Erro ao inserir registro!", Toast.LENGTH_LONG).show();
+                }
+
+
+                if(litiaseBiliar.isChecked()){
+                    if(idade>70){
+                        qtd++;
+                    }
+                    if(leococitos>18000){
+                        qtd++;
+                    }
+                    if(glicemia>12.2){
+                        qtd++;
+                    }
+                    if(ast>250){
+                        qtd++;
+                    }
+                    if(ldh>400){
+                        qtd++;
+                    }
+                }else{
+                    if(idade>55){
+                        qtd++;
+                    }
+                    if(leococitos>16000){
+                        qtd++;
+                    }
+                    if(glicemia>11){
+                        qtd++;
+                    }
+                    if(ast>250){
+                        qtd++;
+                    }
+                    if(ldh>350){
+                        qtd++;
+                    }
+
+                }
+                ////
+
+
+
+
+
+                if(qtd<=2){
+                    mor = 2;
+                }else if(qtd>=3 && qtd<=4){
+                    mor =  15;
+                }else if(qtd==5){
+                    mor = 40;
+                }
+
+                pontuacao.setText("Pontuação:" + qtd);
+                mortalidade.setText("Mortalidade: " + mor+"%");
+            }
+
         });
 
     }
